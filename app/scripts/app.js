ba77605aeba51d60d00fe03aedad465a635c43e2
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2016-01-08 13:43:15 -0500

    Initial Push

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name c3AngularChartApp
+ * @description
+ * # c3AngularChartApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('c3AngularChartApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name c3AngularChartApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the c3AngularChartApp
+ */
+angular.module('c3AngularChartApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
