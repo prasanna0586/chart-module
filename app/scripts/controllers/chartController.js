3f0482edc36f658af82db2b2c8a90b76afc616e1
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2016-01-08 16:33:03 -0500

    C3 angular charts matching athena style

--- app/scripts/controllers/chartController.js
+++ app/scripts/controllers/chartController.js
@@ -7,6 +7,8 @@
  * # MainCtrl
  * Controller of the c3AngularChartApp
  */
-angular.module('c3AngularChartApp').controller('chartCtrl', function () {
+angular.module('c3AngularChartApp').controller('chartCtrl', function ($scope) {
+	$scope.splineYData = [2921447.69,4371204.81,4140342.82,1088671.77];
+	$scope.splineXData = ["Jul2015","Aug2015","Sep2015","Oct2015"];
 
 });
