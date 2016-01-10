7e5690e07b5b56a3b652e553fc9dc0173621da95
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2016-01-10 17:30:12 -0500

    Dynamic charts

--- app/scripts/controllers/chartController.js
+++ app/scripts/controllers/chartController.js
@@ -11,23 +11,17 @@ angular.module('c3AngularChartApp').controller('chartCtrl', function ($scope) {
 	$scope.splineXData = "Jul2015,Aug2015,Sep2015,Oct2015";
 	$scope.splineYData = "2921447.69,4371204.81,4140342.82,1088671.77";
 	
-	/*if ($scope.splineYData) {
-		var tickOne, tickTwo, tickThree;
-		var partsOfStr = $scope.splineYData.split(',');
-		console.log('partsOfStr ' + partsOfStr);
-		console.log('partsOfStr.length ' + partsOfStr.length);
-		var highest = 0;
-		for (var i = 0; i < partsOfStr.length; i++) {
-			if (partsOfStr[i] > highest) {
-				highest = partsOfStr[i];
-			}
-		}
-		console.log('highest ' + highest);
-		tickOne = highest/3;
-		tickTwo = highest*2/3;
-		tickThree = highest;
-		console.log('tickOne ' + tickOne + ' tickTwo ' + tickTwo + ' tickThree ' + tickThree);
-		$scope.splineTickValues = tickOne + "," + tickTwo + "," + tickThree;
-	}*/
+	//The first set of chartData and chartColumns is for a bar chart and the second one is for a donut chart
+	//chartX is not needed for a donut chart
+	
+	//$scope.chartData = [{"x": 1,"data2":2921447.69},{"x": 2,"data2":4371204.81}, {"x": 3,"data2":4140342.82}, {"x": 4,"data2":1088671.77}];
+	$scope.chartData = [{"data1":2921447.69},{"data2":4371204.81}, {"data3":4140342.82}, {"data4":1088671.77}];//Data for donut
+	//$scope.chartColumns = [{"id": "data2", "type": "bar", "name": "Sale_Amount"}];
+	//Columns for donut
+	$scope.chartColumns = [{"id": "data1", "type": "donut", "name": "Aug"},
+	                       {"id": "data2", "type": "donut", "name": "Sep"},
+	                       {"id": "data3", "type": "donut", "name": "Oct"},
+	                       {"id": "data4", "type": "donut", "name": "Nov"}];
+	//$scope.chartX = {"id": "x", "name": "Month"};
 
 });
