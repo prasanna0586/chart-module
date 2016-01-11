035800866f429556cfd215d185a41f6981c28e79
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2016-01-10 23:34:09 -0500

    Chart module category dynamic  chart

--- app/scripts/controllers/chartController.js
+++ app/scripts/controllers/chartController.js
@@ -7,21 +7,27 @@
  * # MainCtrl
  * Controller of the c3AngularChartApp
  */
-angular.module('c3AngularChartApp').controller('chartCtrl', function ($scope) {
-	$scope.splineXData = "Jul2015,Aug2015,Sep2015,Oct2015";
-	$scope.splineYData = "2921447.69,4371204.81,4140342.82,1088671.77";
-	
-	//The first set of chartData and chartColumns is for a bar chart and the second one is for a donut chart
-	//chartX is not needed for a donut chart
-	
-	//$scope.chartData = [{"x": 1,"data2":2921447.69},{"x": 2,"data2":4371204.81}, {"x": 3,"data2":4140342.82}, {"x": 4,"data2":1088671.77}];
-	$scope.chartData = [{"data1":2921447.69},{"data2":4371204.81}, {"data3":4140342.82}, {"data4":1088671.77}];//Data for donut
-	//$scope.chartColumns = [{"id": "data2", "type": "bar", "name": "Sale_Amount"}];
+angular.module('c3AngularChartApp').controller('chartCtrl', function ($scope) {                      
+	 //$scope.chartData = []
+	  
+	  
+	  
+	//Data for donut
+	//$scope.chartData = [{"data1":2921447.69},{"data2":4371204.81}, {"data3":4140342.82}, {"data4":1088671.77}];
 	//Columns for donut
-	$scope.chartColumns = [{"id": "data1", "type": "donut", "name": "Aug"},
-	                       {"id": "data2", "type": "donut", "name": "Sep"},
-	                       {"id": "data3", "type": "donut", "name": "Oct"},
-	                       {"id": "data4", "type": "donut", "name": "Nov"}];
-	//$scope.chartX = {"id": "x", "name": "Month"};
+	/*$scope.chartColumns = [{"id": "data1", "type": "donut", "name": "Aug"},
+		                       {"id": "data2", "type": "donut", "name": "Sep"},
+		                       {"id": "data3", "type": "donut", "name": "Oct"},
+		                       {"id": "data4", "type": "donut", "name": "Nov"}];*/
+	  
+	//Data for bar
+	$scope.chartData = [{"x": "Aug, 2015","data2":2921447.69},
+	                    {"x": "Sep, 2015","data2":4371204.81}, 
+	                    {"x": "Oct, 2015","data2":4140342.82}, 
+	                    {"x":"Nov, 2015","data2":1088671.77}];
+	//Columns for bar
+	$scope.chartColumns = [{"id": "data2", "type": "bar", "name": "Sale_Amount"}];
+	//X axis for bar
+	$scope.chartX = {"id": "x", "name": "Month"};
 
 });
