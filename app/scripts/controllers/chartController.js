3df3edce75a11c30bbf2128d2415fd9c90291d37
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2016-01-14 15:37:51 -0500

    Implemented $http to provide JSON on the fly

--- app/scripts/controllers/chartController.js
+++ app/scripts/controllers/chartController.js
@@ -7,67 +7,24 @@
  * # MainCtrl
  * Controller of the c3AngularChartApp
  */
-angular.module('c3AngularChartApp').controller('chartCtrl', ['$scope', 'chartService', function ($scope, chartService) {
+angular.module('c3AngularChartApp').controller('chartCtrl', ['$scope', 'chartService', '$http', function ($scope, chartService, $http) {
 	
-	$scope.onLoad = function () {
+	$scope.onLoad = function (wsResponse) {
 		$scope.chartData = new Array();
 		$scope.chartColumns = new Array();
 		$scope.chartX = new Object();
-		var wsResponse = chartService.getWSResponse();
+		console.log('wsResponse ctrl');
+		console.log(wsResponse);
 		var chartObject = chartService.getChartData(wsResponse);
 		if (!_.isEmpty(chartObject)) {
 			$scope.chartData = chartObject.data;
 			$scope.chartColumns = chartObject.columns;
-			$scope.chartX = chartObject.xAxis;
-		}
-	};
-	
-	/*$scope.onChartLoad = function () {
-		var chartArrayofObj = $scope.responseFromWebSocket.response.charts;
-		var chartID, chartType, chartData, chartAxis, chartXAxis;
-		$scope.chartData = new Array();
-		$scope.chartColumns = new Array();
-		for (var i = 0; i < chartArrayofObj.length; i++) {
-			chartID = chartArrayofObj[i].chartId;
-			chartType = chartArrayofObj[i].type;
-			chartData = chartArrayofObj[i].chartData;
-			chartAxis = chartData.axis;
-			if (chartAxis && chartAxis.length > 0) {
-				for (var j = 0; j < chartAxis.length; j++) {
-					var chartDataObj = new Object();
-					chartXAxis = chartAxis[j].nodeLabel;
-					chartDataObj.x = chartXAxis;
-					if (chartAxis[j].axis && chartAxis[j].axis.length > 0) {
-						for (var k = 0; k < chartAxis[j].axis.length; k++) {
-							chartDataObj[chartAxis[j].axis[k].nodeLabel] = chartAxis[j].axis[k].nodeValue;		
-							var chartColumnsObj = $scope.createChartColumnObj(chartAxis[j].axis[k], chartType);				
-							$scope.chartColumns.push(chartColumnsObj);
-						}
-					} else {
-						chartDataObj[chartAxis[j].nodeLabel] = chartAxis[j].nodeValue;	
-						var chartColumnsObj = $scope.createChartColumnObj(chartAxis[j], chartType);
-						$scope.chartColumns.push(chartColumnsObj);
-					}
-					$scope.chartData.push(chartDataObj);
-				}
-			}
-			if (chartType != "donut") {
-				$scope.chartX = {"id": "x"};
-			}
+			$scope.chartX = chartObject.xAxis
 		}
 	};
 
-	$scope.createChartColumnObj = function (chartAxis, chartType) {
-		var chartColumnsObj = new Object();
-		var label = chartAxis.nodeLabel;
-		var value = chartAxis.nodeValue;
-
-		chartColumnsObj.id = label;
-		chartColumnsObj.type = chartType;
-		chartColumnsObj.name = label;
-
-		return chartColumnsObj;
-	};*/
-
-	$scope.onLoad();
+	$http.get('/sample_ws_response/bar-chart-with-categories-small.json').success(function (response) {
+		$scope.wsResponse = response;
+		$scope.onLoad($scope.wsResponse);
+	});
 }]);
